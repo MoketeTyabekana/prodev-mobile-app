7b7a100b49c00d68d885ddc93f2567c78b77ea48
Mokete <ematyabekana@gmail.com>
2025-08-27 14:43:07 +0200

    Refactored Index component to use ImageBackground and enhance layout with new text elements

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,21 +1,54 @@
-import { Text, View } from "react-native";
+import {
+  ImageBackground,
+  Dimensions,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+        <ImageBackground
+          source={require("@/assets/images/background-image.png")}
+          style={styles.background}
+          resizeMode="cover"
         >
-          <Text>Edit app/index.tsx to edit this screen.</Text>
-        </View>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image source={require("@/assets/images/Logo.png")} />
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>
+                Find your favorite place here
+              </Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
+          </View>
+        </ImageBackground>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  companyLogo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+});
