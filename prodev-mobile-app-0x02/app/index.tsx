40b22f2a65e3f08ba37a2edd1224316a4e96c386
Mokete <ematyabekana@gmail.com>
2025-08-27 14:48:30 +0200

    Added buttons for user actions and update layout in Index component

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -33,6 +33,22 @@ export default function Index() {
               <Text style={styles.textSmall}>The best prices for over 2 </Text>
               <Text style={styles.textSmall}>million properties worldwide</Text>
             </View>
+
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={{ ...styles.textSmall, color: "black" }}>
+                  Join here
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.transparentButton}>
+                <Text style={styles.textSmall}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{ alignItems: "center", paddingVertical: 20 }}>
+              <Text style={{ color: "white" }}>Continue to home</Text>
+            </View>
           </View>
         </ImageBackground>
       </SafeAreaView>
@@ -41,14 +57,62 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   background: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    width: "100%",
+    height: Dimensions.get("window").height,
   },
   companyLogo: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
+    width: "100%",
+    alignItems: "center",
+    padding: 20,
+    marginBottom: 50,
+  },
+  textGroup: {
+    alignItems: "center",
+  },
+  textLarge: {
+    color: "white",
+    fontWeight: "800",
+    fontSize: 40,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  textSmall: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "200",
+    textAlign: "center",
+  },
+  transparentButton: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    flex: 1,
+  },
+  button: {
+    borderColor: "white",
+    borderWidth: 2,
+    borderRadius: 40,
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    alignItems: "center",
+    fontSize: 20,
+    backgroundColor: "white",
+    flex: 1,
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    gap: 20,
+    paddingHorizontal: 20,
+    width: "100%",
   },
 });
